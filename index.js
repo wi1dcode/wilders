f4d47bd532cf3a23d421f345bb8b52eb1bdce3ea
wi1dcode <magosdk@icloud.com>
2022-09-29 14:56:42 +0200

    exo

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk"
+
+console.log(chalk.blue("Quentin!"))
+console.log(chalk.red("Eric!"))
+console.log(chalk.yellow("Dinh Huy Trinh!"))
+console.log(chalk.green("Dinh Huy Trinh!"))
